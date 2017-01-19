117e735517953d5ad66039b09dd363ccb98c7e12
Wojciech Szczyrba <benice195@gmail.com>
2017-01-19 15:58:20 +0100

    fix pokazywanie poszczegolnych panstw z listy

--- scripts.js
+++ scripts.js
@@ -15,21 +15,23 @@ function searchCountries() {
 function showCountriesList(resp) {
   	countriesList.empty();
     if (resp.length === 1) {
-        resp.forEach(function(item) {
+        var singleCountry = resp[0];
+        
    		$('<div id="country">').appendTo(countriesList)
-           .append($('<h2>').text('Country: ' + item.name))
+           .append($('<h2>').text('Country: ' + singleCountry.name))
            .append($('<h3>').text('Country data:'))
-           .append($('<li>').text('Capital: ' + item.capital))
-           .append($('<li>').text('Region: ' + item.region))
-           .append($('<li>').text('Population: ' + item.population))
-           .append($('<li>').text('Language: ' + item.languages))
-           .append($('<li>').text('Currency: ' + item.currencies))
-        });
+           .append($('<li>').text('Capital: ' + singleCountry.capital))
+           .append($('<li>').text('Region: ' + singleCountry.region))
+           .append($('<li>').text('Population: ' + singleCountry.population))
+           .append($('<li>').text('Language: ' + singleCountry.languages))
+           .append($('<li>').text('Currency: ' + singleCountry.currencies))
+       
         
     } else {
         resp.forEach(function(item) {
-   		$('<div id="country">').appendTo(countriesList)
-           .append($('<h2 id="show">').text('Country: ' + item.name))
+            var divCountry = $('<div id="country">');
+   		    divCountry.appendTo(countriesList)
+           .append($('<h2 class="show">').text('Country: ' + item.name))
            .append($('<h3>').text('Country data:'))
            .append($('<li>').text('Capital: ' + item.capital))
            .append($('<li>').text('Region: ' + item.region))
@@ -38,93 +40,15 @@ function showCountriesList(resp) {
            .append($('<li>').text('Currency: ' + item.currencies))
            $('li').hide();
            $('h3').hide();
-
+           divCountry.click(function() {
+               $(this).find('h3').show();
+               $(this).find('li').show();
+           });
         });
-        ("<h2>").addEventListener("click", hell())
     }
-    function hell(){
-            $("#show").click(function(){
-                $('h3').show();
-                $('li').show();
-            });
-    }
-
 };
 
-      /*
-      
-      <<<<---->>> Nie działa 
 
-      $('<div id="country">').appendTo(countriesList)
-           $("#country").append($('<h2>').text('Country: ' + item.name));
-           $("#country").append($('<h3>').text('Country data:'));
-           $("#country").append($('<li>').text('Capital: ' + item.capital));
-           $("#country").append($('<li>').text('Region: ' + item.region));
-           $("#country").append($('<li>').text('Population: ' + item.population));
-           $("#country").append($('<li>').text('Language: ' + item.languages));
-           $("#country").append($('<li>').text('Currency: ' + item.currencies));
-      
-      $('<div id="country">').appendTo(countriesList)
-           $('<h2>').text('Country: ' + item.name).append($("#country"));
-      <<<--->>>
-      
-
-      
-
-
-        $('<h2>').text('Country: ' + item.name).appendTo(countriesList);
-        $('<h3>').text('Country data:').appendTo(countriesList);
-        $('<li>').text('Capital: ' + item.capital).appendTo(countriesList);
-        $('<li>').text('Region: ' + item.region).appendTo(countriesList);
-        $('<li>').text('Population: ' + item.population).appendTo(countriesList);
-        $('<li>').text('Language: ' + item.languages).appendTo(countriesList);
-        $('<li>').text('Currency: ' + item.currencies).appendTo(countriesList);
-        
-        >>>>>>>>>>>>>>>>>>>>>> dzialajaca strona
-
-
-        var url = 'https://restcountries.eu/rest/v1/name/';
-
-var countriesList = $('#countries');
-$('#search').click(searchCountries);
-
-function searchCountries() {
- 	var countryName = $('#country-name').val();
-    if(!countryName.length) countryName = 'Poland';
-    $.ajax({
-            url: url + countryName,
-            method: 'GET',
-            success: showCountriesList
-        });
-    };
-function showCountriesList(resp) {
-  	countriesList.empty();
-    if (resp.length === 1) {
-        resp.forEach(function(item) {
-   		$('<div id="country">').appendTo(countriesList)
-           .append($('<h2>').text('Country: ' + item.name))
-           .append($('<h3>').text('Country data:'))
-           .append($('<li>').text('Capital: ' + item.capital))
-           .append($('<li>').text('Region: ' + item.region))
-           .append($('<li>').text('Population: ' + item.population))
-           .append($('<li>').text('Language: ' + item.languages))
-           .append($('<li>').text('Currency: ' + item.currencies))
-        });
-    } else {
-        resp.forEach(function(item) {
-   		$('<div id="country">').appendTo(countriesList)
-           .append($('<h2 id="show">').text('Country: ' + item.name))
-        });
-
-    }
-
-};
-
-<<<<<<<<<<<<<<<<<<<<< koniec dzialajacej strony
-        
-        
-        
-        
         
         
         
